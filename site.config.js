0b77ff345d2bf1eae26b4cba849742e07134cce5
EquivalentFlow <125655867+EquivalentFlow@users.noreply.github.com>
2023-02-17 11:20:04 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "EquivalentFlow",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/m87-full_jpg.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
