b9b15a46df02d58011e5c69ca160e3149b119c4c
EquivalentFlow <125655867+EquivalentFlow@users.noreply.github.com>
2023-03-18 00:45:16 +0000

    Update ServiceCard.tsx

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -7,12 +7,8 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <div className="p-1 mb-3 dark:text-white">Resources</div>
-        <a
-          href={`${CONFIG.projects[0].href}`}
-          rel="noreferrer"
-          target="_blank"
-          className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
-        >
+            <ul className="rounded-2xl mb-9 bg-white dark:bg-zinc-700 p-1 flex flex-col">
+        
           <AiFillCodeSandboxCircle className="text-2xl" />
           <div className="text-sm">{CONFIG.projects[0].name}</div>
         </a>
