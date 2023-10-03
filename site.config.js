4cf4675e6b109c76c49ae440f728ba46a874bdfb
EquivalentFlow <125655867+EquivalentFlow@users.noreply.github.com>
2023-10-02 22:19:34 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,7 +10,12 @@ const CONFIG = {
     github: "",
     instagram: "",
   },
-
+projects: [
+    {
+      name: `Resources`,
+      href: "https://linktr.ee/m87space?utm_source=linktree_admin_share",
+    },
+  ],
   // blog setting (required)
   blog: {
     title: "M87",
@@ -18,7 +23,7 @@ const CONFIG = {
     theme: "light", // ['light', 'dark', 'auto']
   },
 
-  // CONFIG configuration (required)
+  // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
@@ -61,6 +66,5 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-};
-
-module.exports = CONFIG;
+}
+module.exports = CONFIG
